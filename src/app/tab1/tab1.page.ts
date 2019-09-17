4ab2544d4ad4f97d5d6d15724fd7024468a5c90e
RaphaelSouza <raphael-souza@hotmail.com>
2019-09-17 18:54:31 -0300

    Commit 1

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+
+  async exibirAlerta() {
+    const alert = await this.alertController.create({
+      header: 'Aula',
+      subHeader: 'Aula 2',
+      message: 'Mensage padrão.',
+      buttons: ['RECUSAR', 'ACEITAR']
+    });
+
+    await alert.present();
+  }
 }
